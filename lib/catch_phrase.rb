6134d989b3684f9694a58976872e7e725c9be064
Patricia C <patricia@patriciaco.com>
2020-01-31 05:20:25 +0000

    Done.

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,14 +1,7 @@
-def catch_phrase
- phrase = "It's-a me, Mario!"
- puts phrase
+def catch_phrase(phrase)
+  puts phrase
+  phrase = "It's-a me, Mario!"
 end
- catch_phrase
-   
-    #"spec_helper"
- #def "#catch_phrase"(phrase) do
-  #it "puts out a catch phrase" do
-  #phrase = "It's-a me, Mario!"
-  #expect{catch_phrase}.to output(phrase)}.to_stdout
- #end
+
 
 
